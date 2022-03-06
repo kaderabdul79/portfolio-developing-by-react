feebcc8dfb704b9e6efb14e3d49b9cff9330a420
kaderabdul <kaderabdul7980@gmail.com>
2022-03-06 14:15:25 +0600

    create necessary components and import in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import Header from './components/header/Header';
+import About from './components/about/About';
+import Contact from './components/contact/Contact';
+import Experience from './components/experience/Experience';
+import Footer from './components/footer/Footer';
+import Nav from './components/nav/Nav';
+import Portfolio from './components/portfolio/Portfolio';
 
 const App = () => {
     return (
         <div>
             hello kader!
+            <Header />
+            <Nav />
+            <Experience />
+            <Portfolio />
+            <About />
+            <Contact />
+            <Footer />
         </div>
     );
 };
