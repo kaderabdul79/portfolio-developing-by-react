3c0542a61074ea9924288a73d3651e8487cd1221
kaderabdul <kaderabdul7980@gmail.com>
2022-03-07 20:36:58 +0600

    reset the files folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,14 @@
 import React from 'react';
-import Header from './components/header/Header';
-import About from './components/about/About';
-import Contact from './components/contact/Contact';
-import Experience from './components/experience/Experience';
-import Footer from './components/footer/Footer';
-import Nav from './components/nav/Nav';
-import Portfolio from './components/portfolio/Portfolio';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Hero from './components/hero/Hero';
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Header></Header>
-                <Hero></Hero>
-                <About />
                  <Switch>
                     {/* <Route exact path="/"></Route> */}
                 </Switch>
             </BrowserRouter>
-            
-            {/* <Nav />
-            <Experience />
-            <Portfolio />
-            <About />
-            <Contact />
-            <Footer /> */}
         </div>
     );
 };
